d3b850c48c027abe666c38581ba76320c18e242c
Jaime691 <jmayorgadev@gmail.com>
2024-10-04 16:34:43 -0500

    SignIn added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { UsersRepository } from './users.repository';
 import { AuthCredentialsDto } from './dto/auth-credentiasls.dto';
 
@@ -12,5 +13,16 @@ export class AuthService {
 
   signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOneBy({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('Please check your login credentials.');
+    }
   }
 }
