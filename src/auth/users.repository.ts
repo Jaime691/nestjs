ccfbf0ce466908fb4d090a8aca1ec235c78b77d3
Jaime691 <jmayorgadev@gmail.com>
2024-10-04 15:59:25 -0500

    hashing password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -4,6 +4,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentiasls.dto';
 
@@ -17,9 +18,12 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = await this.create({
       username,
-      password,
+      password: hashedPassword,
     });
     try {
       await this.save(user);
